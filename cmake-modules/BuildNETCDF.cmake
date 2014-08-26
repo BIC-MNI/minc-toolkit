b8070eb7cf127fd4347b4ad4880d0ffd0970e657
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2014-08-26 11:43:23 -0400

    Updated subproject building rules

--- cmake-modules/BuildNETCDF.cmake
+++ cmake-modules/BuildNETCDF.cmake
@@ -1,5 +1,14 @@
 macro(build_netcdf install_prefix staging_prefix)
 
+  IF(CMAKE_BUILD_TYPE STREQUAL Release)
+    SET(EXT_C_FLAGS   "${CMAKE_C_FLAGS}   ${CMAKE_C_FLAGS_RELEASE}")
+    SET(EXT_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_RELEASE}")
+  ELSE()
+    SET(EXT_C_FLAGS   "${CMAKE_C_FLAGS}    ${CMAKE_C_FLAGS_DEBUG}")
+    SET(EXT_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGS_DEBUG}")
+  ENDIF()
+
+
 ExternalProject_Add(NETCDF 
   SOURCE_DIR NETCDF
   URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.2.tar.gz"
@@ -8,8 +17,7 @@ ExternalProject_Add(NETCDF
   INSTALL_DIR     "${staging_prefix}"
   BUILD_COMMAND   make 
   INSTALL_COMMAND make DESTDIR=${staging_prefix} install 
-  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-doxygen --disable-hdf4 --disable-netcdf-4 --disable-shared --disable-dap  --libdir=${install_prefix}/lib${LIB_SUFFIX} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
-#  INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/external
+  CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-doxygen --disable-hdf4 --disable-netcdf-4 --disable-shared --disable-dap  --libdir=${install_prefix}/lib${LIB_SUFFIX} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} "CXXFLAGS=${EXT_CXX_FLAGS}" "CFLAGS=${EXT_C_FLAGS}"
 )
 
 SET(NETCDF_LIBRARY     ${staging_prefix}/${install_prefix}/lib${LIB_SUFFIX}/libnetcdf.a )
